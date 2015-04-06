db1134aa733da35ba73fd0d46e1a9d56d37fcdb9
Yan Lhert <info@yanlhert.com>
2015-04-06 11:05:02 -0700

    removing some unused code

---
+++ test/dummy/app/controllers/blah_controller.rb
@@ -0,0 +1,8 @@
+class BlahController < ApplicationController
+  def index
+  end
+
+  def blah1
+    render :index
+  end
+end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'blah/' => 'blah#index'
+  get 'blah1/' => 'blah#blah1'
+
   get '/nodesc' => 'home#no_description'
 
   root to: "home#index"
